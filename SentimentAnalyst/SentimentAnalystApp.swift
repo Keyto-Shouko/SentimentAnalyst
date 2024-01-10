36d2379a5305954e125f691d3e625971bbf8944f
Bruno <balvesca@gmail.com>
2024-01-10 15:38:30 +0100

    Initial Commit

---
+++ SentimentAnalyst/SentimentAnalystApp.swift
@@ -0,0 +1,17 @@
+//
+//  SentimentAnalystApp.swift
+//  SentimentAnalyst
+//
+//  Created by ALVES CARVALHO Bruno on 10/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct SentimentAnalystApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
