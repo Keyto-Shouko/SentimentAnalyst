9f0906c8fb8355a2b66877ce46489252ed9ce9a8
Bruno <balvesca@gmail.com>
2024-01-10 16:32:12 +0100

    created IA app

--- SentimentAnalyst/ContentView.swift
+++ SentimentAnalyst/ContentView.swift
@@ -7,18 +7,62 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+enum Sentiment: String {
+    case positive = "POSITIVE"
+    case negative = "NEGATIVE"
+    case mixed = "MIXED"
+    case neutral = "NEUTRAL"
+    
+    func getEmoji(self: Sentiment) -> String {
+        switch self {
+        case .positive:
+            return "😀"
+        case .negative:
+            return "😠"
+        case .mixed:
+            return "🤔"
+        case .neutral:
+            return "😐"
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView: View {
+    
+    @State var modelInput : String = "ouaiiii"
+    @State var outputSentiment : Sentiment = .mixed
+    
+    var body: some View {
+        NavigationStack{
+            VStack {
+                VStack(alignment: .leading, spacing: 18) {
+                    Text("Entrez un texte et l'IA devinera le sentiment (normalement)")
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        .fontDesign(.monospaced)
+                        .foregroundStyle(.yellow)
+                    TextField("Enter text", text: $modelInput)
+                                        .padding()
+                                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    Button(action: {
+                        
+                    }, label: {
+                        Text("Ajouter")
+                    })
+                    
+                }
+                .background(Color.purple)
+                VStack (alignment: .leading, spacing: 18){
+                    Text($outputSentiment).background(Color.cyan)
+                }
+                
+            }
+            .padding()
+        }
+        .navigationBarTitle("Sentiment Analysis")
+    }
+    
+}
+
+#Preview{
+    ContentView(modelInput: "LEZGO CHELSEA", outputSentiment: .positive)
 }
